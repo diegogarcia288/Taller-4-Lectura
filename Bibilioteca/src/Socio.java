b91326baa775671e2850d328174cda06bb8b1905
cnway.git <cway5338@gmail.com>
2026-09-23 20:10:26 -0500

    Add files via upload

---
+++ Bibilioteca/src/Socio.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author User
+ */
+public class Socio {
+    public String nombre;
+    private int numcarnet;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getNumcarnet() {
+        return numcarnet;
+    }
+
+    public void setNumcarnet(int numcarnet) {
+        this.numcarnet = numcarnet;
+    }
+    
+    
+    
+}
